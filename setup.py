bbe21f6c5004449f862da5fa600434be95bc3535
Samuel D Lotz <samuel.lotz@salotz.info>
2019-06-07 15:32:29 -0400

    updating build env etc for new minor release and deployment

--- setup.py
+++ setup.py
@@ -1,18 +1,55 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import print_function
+
 from setuptools import setup, find_packages
 from Cython.Build import cythonize
 
 import numpy as np
 
+import itertools as it
+
+# setuptools only specifies abstract requirements. For the concrete
+# requirements i.e. index or repo URL see requirements.txt
+abstract_requirements = [
+    'numpy',
+    'scipy',
+]
+
+
 setup(
     name='geomm',
     version='0.1.6',
-    py_modules=['geomm'],
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=[
-        'numpy',
-        'scipy'
+    author="Samuel D. Lotz",
+    author_email="samuel.lotz@salotz.info",
+    description="A simple no-nonsense library for computing common geometry on macromolecular systems.",
+    long_description=open('README.org').read(),
+    license="MIT",
+    url="https://github.com/ADicksonLab/geomm",
+    classifiers=[
+        "Development Status :: 3 - Beta",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+        'Programming Language :: Python :: 3'
     ],
+
+    # building/dev
+    setup_requires=['pytest-runner', 'numpy', 'cython'],
+    tests_require=['pytest', 'tox'],
+    install_requires=abstract_requirements,
     include_dirs=[np.get_include()],
-    ext_modules = cythonize("geomm/pyqcprot.pyx")
+    ext_modules = cythonize("src/geomm/pyqcprot.pyx"),
+
+    # package
+    packages=find_packages('src'),
+
+    package_dir={'' : 'src'},
+
+    # if this is true then the package_data won't be included in the
+    # dist, and I prefer this to MANIFEST
+    include_package_data=False,
+
+
 )
--- sphinx/build.sh
+++ sphinx/build.sh
@@ -1,16 +1,18 @@
-# run pandoc on all the handwritted documentation files to produce rst
-# from org files
-rm source/*~* &> /dev/null
-for f in $(ls source); do
-    echo "converting source/$f to rst"
-    pandoc -f org -t rst -o source/${f%.org}.rst source/$f
+#!/bin/sh
+
+# run pandoc to produce rst from org-mode files
+for f in source/*.org; do
+    echo "converting $f to rst"
+    f_name=$(basename $f)
+    pandoc -f org -t rst -o source/${f_name%.org}.rst $f
 done;
-rm source/*.rst.rst* &> /dev/null
 
 # now build the API docs automatically from the source
 
 # also build locally in sphinx for easier testing
 make clean
+# clean the api docs first
+rm -rf api/*
 # generate the api rst files for autodoc
-sphinx-apidoc -f --separate --module-first --maxdepth 1 -o api ../geomm
+sphinx-apidoc -f --separate --private --ext-autodoc --module-first --maxdepth 1 -o api ../src/wepy
 sphinx-build -b html -E -a -j 6 . ./_build/html/
