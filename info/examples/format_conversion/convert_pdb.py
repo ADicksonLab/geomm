691ad10145d059b22ba9fedf94d3730ea4073aa2
Samuel Lotz <samuel.lotz@salotz.info>
2020-03-03 18:05:46 -0500

    adds example for bonds and coordinates

--- info/examples/format_conversion/convert_pdb.py
+++ info/examples/format_conversion/convert_pdb.py
@@ -5,7 +5,10 @@ import os.path as osp
 import shutil
 
 import sqlite3
+
+import numpy as np
 import pandas as pd
+import tables as pytables
 import mdtraj as mdj
 from wepy.util.mdtraj import mdtraj_to_json_topology
 from wepy.util.json_top import json_top_chain_df, json_top_residue_df, json_top_atom_df
@@ -28,10 +31,22 @@ fasta_str = traj.top.to_fasta(chain=0)
 with open('outputs/lysozyme_pxylene.res.fasta', 'w') as wf:
     wf.write(fasta_str)
 
-# topology tables
+## topology tables
 
-# you can get a table using mdtraj
+# Bonds
+
+# you can get a table using mdtraj, but we just use the bonds here
 mdj_atoms_df, bonds = traj.top.to_dataframe()
+
+# just the first two columns (atom indices) for our purposes
+bonds = bonds[:,0:2]
+
+# we can just write this multiple ways with numpy
+np.savetxt('outputs/lysozyme_pxylene.bonds.npy_txt', bonds)
+np.save('outputs/lysozyme_pxylene.bonds.npy', bonds)
+
+# make a pandas data frame
+bond_df = pd.DataFrame(bonds)
 
 # but wepy provides the ability to get normalized versions for each
 # level
@@ -39,6 +54,7 @@ chain_df = json_top_chain_df(json_top)
 residue_df = json_top_residue_df(json_top)
 atom_df = json_top_atom_df(json_top)
 
+bond_df.to_csv('outputs/lysozyme_pxylene.bond.csv', index=False)
 chain_df.to_csv('outputs/lysozyme_pxylene.chain.csv', index=False)
 residue_df.to_csv('outputs/lysozyme_pxylene.residue.csv', index=False)
 atom_df.to_csv('outputs/lysozyme_pxylene.atom.csv', index=False)
@@ -47,11 +63,13 @@ atom_df.to_csv('outputs/lysozyme_pxylene.atom.csv', index=False)
 # to an SQLite3 database
 db = sqlite3.Connection("outputs/lysozyme_pxylene.sqlite3")
 
+bond_df.to_sql('bonds', db)
 chain_df.to_sql('chains', db)
 residue_df.to_sql('residues', db)
 atom_df.to_sql('atoms', db)
 
 # to an HDF5 file
+bond_df.to_hdf('outputs/lysozyme_pxylene.top.h5', 'bonds')
 chain_df.to_hdf('outputs/lysozyme_pxylene.top.h5', 'chains')
 residue_df.to_hdf('outputs/lysozyme_pxylene.top.h5', 'residues')
 atom_df.to_hdf('outputs/lysozyme_pxylene.top.h5', 'atoms')
@@ -59,6 +77,15 @@ atom_df.to_hdf('outputs/lysozyme_pxylene.top.h5', 'atoms')
 # to an excel spreadsheet
 with pd.ExcelWriter('outputs/lysozyme_pxylene.top.xlsx', mode='r+') as writer:
 
+    bond_df.to_excel(writer, sheet_name='bonds')
     chain_df.to_excel(writer, sheet_name='chains')
     residue_df.to_excel(writer, sheet_name='residues')
     atom_df.to_excel(writer, sheet_name='atoms')
+
+
+## coordinates
+
+# separately, in binary format
+coords = traj.xyz
+
+np.savez('lysozyme_pxylene_reference.npz', coords)
