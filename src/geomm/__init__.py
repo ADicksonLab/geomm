b8644aeb440fa7e08982038dbabc9dcd35730dcd
salotz <salotz@salotz.info>
2020-08-07 14:08:01 -0400

    updating to versioneer

--- src/geomm/__init__.py
+++ src/geomm/__init__.py
@@ -2,7 +2,9 @@
 
 """Top-level package."""
 
-from ._version import __version__
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
 
 __author__ = "Samuel D. Lotz"
 __email__ = 'samuel.lotz@salotz.info'
