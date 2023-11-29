fe377fd9fa4a4951ff0474a0620f8f05c5c937da
salotz <salotz@salotz.info>
2023-11-28 21:07:21 -0500

    update versioneer
    Was causing problems when installing from existing version

--- src/geomm/__init__.py
+++ src/geomm/__init__.py
@@ -2,9 +2,8 @@
 
 """Top-level package."""
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
 
 __author__ = "Samuel D. Lotz"
 __email__ = 'samuel.lotz@salotz.info'
